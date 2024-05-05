7a263cc8ce19b367c3222b2f7a1d3c9be4c9c07b
Talk Is Cheap <779222056@qq.com>
2024-05-05 19:43:34 +0800

    Add files via upload

---
+++ src/config.lua
@@ -0,0 +1,53 @@
+
+-- 0 - disable debug info, 1 - less debug info, 2 - verbose debug info
+DEBUG = 0
+
+-- display FPS stats on screen
+DEBUG_FPS = false
+
+-- dump memory info every 10 seconds
+DEBUG_MEM = false
+
+-- load deprecated API
+LOAD_DEPRECATED_API = false
+
+-- load shortcodes API
+LOAD_SHORTCODES_API = true
+
+-- screen orientation
+CONFIG_SCREEN_ORIENTATION = "landscape"
+
+-- design resolution
+CONFIG_SCREEN_WIDTH  = 800
+-- CONFIG_SCREEN_WIDTH  = 960
+CONFIG_SCREEN_HEIGHT = 533
+
+-- auto scale mode
+CONFIG_SCREEN_AUTOSCALE = "FIXED_WIDTH"
+
+-- 是否显示calcAttackDamge和calcMagicDamge
+DEBUG_SHOW_DAMGEINFO = false
+
+-- 是否显示AiUtils中的打印
+DEBUG_SHOW_AI_UTILS = false
+
+-- 是否显示R剧本中的网格
+DEBUG_SHOW_RPLOT_GRID = false
+
+-- 是否打印地图上的点的信息
+DEBUG_MAP_VIEW_INFO = false
+
+-- 是否合并对话到一个命令
+RPLOT_UNION_DIALOG = false
+
+-- 是否合并连续的SceneDialog命令
+RPLOT_UNION_SCENE_DIALOG = true
+
+-- 是否合并连续的RoleMove命令，这个是必须的，因为原版的不合并，单个就会无法一起移动
+RPLOT_UNION_ROLEMOVE = true
+
+-- 是否显示忽略命令的注释
+PLOT_NEED_SHOW_IGNORE_CMD = false
+
+-- R剧本中快速测试，不显示各种对话框
+RPLOT_QUICK_TEST = false
---
+++ src/main.lua
@@ -0,0 +1,18 @@
+
+function __G__TRACKBACK__(errorMessage)
+    print("----------------------------------------")
+    print("LUA ERROR: " .. tostring(errorMessage) .. "\n")
+    print(debug.traceback("", 2))
+    print("----------------------------------------")
+
+    --!! 1、错误信息可以发送给服务器，目前没有
+
+    -- 2、错误信息显示在游戏内，这个仅在开发过程中
+    require("module.ui.error_panel").new(tostring(errorMessage), debug.traceback("", 2))
+end
+--cc.Texture2D:setDefaultAlphaPixelFormat(0x8)
+--cc.Director:getInstance():setAnimationInterval(1.0 / 40)
+package.path = package.path .. ";src/"
+cc.FileUtils:getInstance():setPopupNotify(false)
+cc.FileUtils:getInstance():addSearchPath("res/ccz/ui/") -- 坑爹，cocos studio中引用了其他csd，则需要将其引用的csd放入res根目录下，否则则需要添加搜索路径
+require("app.MyApp").new():run()
